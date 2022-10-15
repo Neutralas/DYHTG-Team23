b3c006174a1d5029811d4070f43d05c9d0cf7319
Civan Dogan <ciwoerim@gmail.com>
2022-10-15 23:10:50 +0100

    merge

--- zombie_texture_generator/main.py
+++ zombie_texture_generator/main.py
@@ -1,13 +1,15 @@
 
 from FaceExtractor import FaceExtractor
-
+from image_generation import ImageGen
 
 if __name__ == '__main__':
 
-
+    ImageGen.run()
     f = FaceExtractor('images/')
     f.run()
     #f.save_faces()
     f.save_face_on_minecraft_zombie()
+
+
 
 
